7b53d7cfc766c3918825bd138c46f551bb04a3a6
Markus Ebner <hiwatari.seiji@gmail.com>
2020-05-29 21:11:28 +0200

    Move svm_load_model implementation onto new abstracted c++ parser

--- svm.cpp
+++ svm.cpp
@@ -8,6 +8,7 @@
 #include <limits.h>
 #include <locale.h>
 #include <thread>
+#include <fstream>
 #include <sstream>
 #include <vector>
 #include "svm.h"
@@ -2744,257 +2745,6 @@ int svm_save_model(const char *model_file_name, const svm_model *model)
 	else return 0;
 }
 
-static char *line = NULL;
-static int max_line_len;
-
-static char* readline(FILE *input)
-{
-	int len;
-
-	if(fgets(line,max_line_len,input) == NULL)
-		return NULL;
-
-	while(strrchr(line,'\n') == NULL)
-	{
-		max_line_len *= 2;
-		line = (char *) realloc(line,max_line_len);
-		len = (int) strlen(line);
-		if(fgets(line+len,max_line_len-len,input) == NULL)
-			break;
-	}
-	return line;
-}
-
-//
-// FSCANF helps to handle fscanf failures.
-// Its do-while block avoids the ambiguity when
-// if (...)
-//    FSCANF();
-// is used
-//
-#define FSCANF(_stream, _format, _var) do{ if (fscanf(_stream, _format, _var) != 1) return false; }while(0)
-bool read_model_header(FILE *fp, svm_model* model)
-{
-	svm_parameter& param = model->param;
-	// parameters for training only won't be assigned, but arrays are assigned as NULL for safety
-	param.nr_weight = 0;
-	param.weight_label = NULL;
-	param.weight = NULL;
-
-	char cmd[81];
-	while(1)
-	{
-		FSCANF(fp,"%80s",cmd);
-
-		if(strcmp(cmd,"svm_type")==0)
-		{
-			FSCANF(fp,"%80s",cmd);
-			int i;
-			for(i=0;svm_type_table[i];i++)
-			{
-				if(strcmp(svm_type_table[i],cmd)==0)
-				{
-					param.svm_type=i;
-					break;
-				}
-			}
-			if(svm_type_table[i] == NULL)
-			{
-				fprintf(stderr,"unknown svm type.\n");
-				return false;
-			}
-		}
-		else if(strcmp(cmd,"kernel_type")==0)
-		{
-			FSCANF(fp,"%80s",cmd);
-			int i;
-			for(i=0;kernel_type_table[i];i++)
-			{
-				if(strcmp(kernel_type_table[i],cmd)==0)
-				{
-					param.kernel_type=i;
-					break;
-				}
-			}
-			if(kernel_type_table[i] == NULL)
-			{
-				fprintf(stderr,"unknown kernel function.\n");
-				return false;
-			}
-		}
-		else if(strcmp(cmd,"degree")==0)
-			FSCANF(fp,"%d",&param.degree);
-		else if(strcmp(cmd,"gamma")==0)
-			FSCANF(fp,"%lf",&param.gamma);
-		else if(strcmp(cmd,"coef0")==0)
-			FSCANF(fp,"%lf",&param.coef0);
-		else if(strcmp(cmd,"nr_class")==0)
-			FSCANF(fp,"%d",&model->nr_class);
-		else if(strcmp(cmd,"total_sv")==0)
-			FSCANF(fp,"%d",&model->l);
-		else if(strcmp(cmd,"rho")==0)
-		{
-			int n = model->nr_class * (model->nr_class-1)/2;
-			model->rho = Malloc(double,n);
-			for(int i=0;i<n;i++)
-				FSCANF(fp,"%lf",&model->rho[i]);
-		}
-		else if(strcmp(cmd,"label")==0)
-		{
-			int n = model->nr_class;
-			model->label = Malloc(int,n);
-			for(int i=0;i<n;i++)
-				FSCANF(fp,"%d",&model->label[i]);
-		}
-		else if(strcmp(cmd,"probA")==0)
-		{
-			int n = model->nr_class * (model->nr_class-1)/2;
-			model->probA = Malloc(double,n);
-			for(int i=0;i<n;i++)
-				FSCANF(fp,"%lf",&model->probA[i]);
-		}
-		else if(strcmp(cmd,"probB")==0)
-		{
-			int n = model->nr_class * (model->nr_class-1)/2;
-			model->probB = Malloc(double,n);
-			for(int i=0;i<n;i++)
-				FSCANF(fp,"%lf",&model->probB[i]);
-		}
-		else if(strcmp(cmd,"nr_sv")==0)
-		{
-			int n = model->nr_class;
-			model->nSV = Malloc(int,n);
-			for(int i=0;i<n;i++)
-				FSCANF(fp,"%d",&model->nSV[i]);
-		}
-		else if(strcmp(cmd,"SV")==0)
-		{
-			while(1)
-			{
-				int c = getc(fp);
-				if(c==EOF || c=='\n') break;
-			}
-			break;
-		}
-		else
-		{
-			fprintf(stderr,"unknown text in model file: [%s]\n",cmd);
-			return false;
-		}
-	}
-
-	return true;
-
-}
-
-svm_model *svm_load_model(const char *model_file_name)
-{
-	FILE *fp = fopen(model_file_name,"rb");
-	if(fp==NULL) return NULL;
-
-	char *old_locale = setlocale(LC_ALL, NULL);
-	if (old_locale) {
-		old_locale = strdup(old_locale);
-	}
-	setlocale(LC_ALL, "C");
-
-	// read parameters
-
-	svm_model *model = Malloc(svm_model,1);
-	model->rho = NULL;
-	model->probA = NULL;
-	model->probB = NULL;
-	model->sv_indices = NULL;
-	model->label = NULL;
-	model->nSV = NULL;
-
-	// read header
-	if (!read_model_header(fp, model))
-	{
-		fprintf(stderr, "ERROR: fscanf failed to read model\n");
-		setlocale(LC_ALL, old_locale);
-		free(old_locale);
-		free(model->rho);
-		free(model->label);
-		free(model->nSV);
-		free(model);
-		return NULL;
-	}
-
-	// read sv_coef and SV
-
-	int elements = 0;
-	long pos = ftell(fp);
-
-	max_line_len = 1024;
-	line = Malloc(char,max_line_len);
-	char *p,*endptr,*idx,*val;
-
-	while(readline(fp)!=NULL)
-	{
-		p = strtok(line,":");
-		while(1)
-		{
-			p = strtok(NULL,":");
-			if(p == NULL)
-				break;
-			++elements;
-		}
-	}
-	elements += model->l;
-
-	fseek(fp,pos,SEEK_SET);
-
-	int m = model->nr_class - 1;
-	int l = model->l;
-	model->sv_coef = Malloc(double *,m);
-	int i;
-	for(i=0;i<m;i++)
-		model->sv_coef[i] = Malloc(double,l);
-	model->SV = Malloc(svm_node*,l);
-	svm_node *x_space = NULL;
-	if(l>0) x_space = Malloc(svm_node,elements);
-
-	int j=0;
-	for(i=0;i<l;i++)
-	{
-		readline(fp);
-		model->SV[i] = &x_space[j];
-
-		p = strtok(line, " \t");
-		model->sv_coef[0][i] = strtod(p,&endptr);
-		for(int k=1;k<m;k++)
-		{
-			p = strtok(NULL, " \t");
-			model->sv_coef[k][i] = strtod(p,&endptr);
-		}
-
-		while(1)
-		{
-			idx = strtok(NULL, ":");
-			val = strtok(NULL, " \t");
-
-			if(val == NULL)
-				break;
-			x_space[j].index = (int) strtol(idx,&endptr,10);
-			x_space[j].value = strtod(val,&endptr);
-
-			++j;
-		}
-		x_space[j++].index = -1;
-	}
-	free(line);
-
-	setlocale(LC_ALL, old_locale);
-	free(old_locale);
-
-	if (ferror(fp) != 0 || fclose(fp) != 0)
-		return NULL;
-
-	model->free_sv = 1;	// XXX
-	return model;
-}
-
 void svm_free_model_content(svm_model* model_ptr)
 {
 	if(model_ptr->free_sv && model_ptr->l > 0 && model_ptr->SV != NULL)
@@ -3197,6 +2947,39 @@ void svm_set_print_string_function(void (*print_func)(const char *))
 
 
 
+class SVMModelParserFileSource {
+	std::ifstream buffer;
+
+public:
+	SVMModelParserFileSource(const char* file_path) : buffer(file_path) {}
+
+	bool read_next(std::string& line) {
+		line.clear();
+		char c = 0;
+		while(buffer.read(&c, 1)) {
+			if(c != '\n' && c != ' ') { line.push_back(c); }
+			else { return line.size() > 0; }
+		}
+		return false;
+	}
+	bool read_line(std::string& line) {
+		return static_cast<bool>(std::getline(buffer, line));
+	}
+	template<typename TValue> bool get(TValue& value, char delim = '\n') {
+		if(buffer >> value) {
+			buffer.ignore(1, delim);
+			return true;
+		}
+		return false;
+	}
+	template<typename TValue> bool get_array(TValue* value, size_t len) {
+		for(size_t i = 0; i < len; ++i) {
+			if(!get(value[i], ' ')) { return false; }
+		}
+		return true;
+	}
+};
+
 class SVMModelParserBufferSource {
 	std::istringstream buffer;
 
@@ -3347,8 +3130,8 @@ private:
 			}
 
 			// parse SVs
-			while(line.good()) {
-				exceptAssert(line >> node_buffer.index && line.ignore(1,':') && line >> node_buffer.value, "Failed to read support vector");
+			while(line.good() && line >> node_buffer.index) {
+				exceptAssert(line.ignore(1,':') && line >> node_buffer.value, "Failed to read support vector");
 				sv_buffer.push_back(node_buffer);
 			}
 			sv_buffer.push_back({.index = -1, .value = 0.0});
@@ -3372,6 +3155,12 @@ private:
 		model->free_sv = 1; // XXX
 	}
 };
+
+svm_model* svm_load_model(const char* model_file_name) {
+	SVMModelParser<SVMModelParserFileSource> parser(model_file_name);
+	if(!parser.parse()) { return nullptr; }
+	return parser.get_model();
+}
 
 svm_model* svm_parse_model_from_buffer(const char* model_buffer, unsigned int length) {
 	SVMModelParser<SVMModelParserBufferSource> parser({model_buffer, length});
